b780146cbf55becdeb89b14891d6e4de86482c5e
Paige <prhoward97@gmail.com>
2022-09-26 23:16:01 -0400

    commit tree 8fc82b3cd396ed7be611a132654bb06579da828b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+   
+}
+
+function setBestCustomer() {
+    return globalThis.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'karen';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'bob';
+}
